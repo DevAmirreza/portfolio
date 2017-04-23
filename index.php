36f3e101cef048bda3c09146eae78ba7c13e8899
Amirreza <atnniya@gmail.com>
2017-04-22 23:42:52 -0400

    implementing intiale design

--- index.php
+++ index.php
@@ -8,16 +8,19 @@ echo $msg ;
 <html>
 <head>
 <title><?php app_display($title) ; ?></title>
-<link rel="stylesheet" href="/styles/template.css"  />
+<link rel="stylesheet" href="styles/template.css"  />
 </head>
 <body>
- <div class="header">
+<div class="container">
+ <div class="header ">
  <?php app_display($title) ; ?>
  
  </div>
  
+ <div class="left mobile"></div>
+ <div class="right mobile"></div>
  
- 
+ </div>
  
    </body>
 
