8b2e9dd6d26043873b266683c6f21a2ca6ab829c
Amirreza <atnniya@gmail.com>
2017-04-22 22:29:35 -0400

    added new content

--- index.php
+++ index.php
@@ -1,3 +1,18 @@
 <?php
-echo "hello world" ; 
+$title =  "Amirreza Portfolio" ; 
+function app_display($msg) {
+echo $msg ; 
+}
+
 ?>
+<html>
+<head>
+<title><?php app_display($title) ; ?></title>
+</head>
+<body>
+ <h1><?php app_display($title) ; ?></h1>
+ 
+   </body>
+
+
+</html>
