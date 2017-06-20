9067e21ee49fb8153a84d5acbc3d3057cba569eb
Amirreza <atnniya@gmail.com>
2017-06-19 20:52:08 -0400

    Added Swipe functionality [Experiment]
    - Also Fixed couple of small issues

--- scripts/timeline/timeline.js
+++ scripts/timeline/timeline.js
@@ -33,6 +33,8 @@ let elem = $(".point") ;
 // }) ;
 
 
+// TODO: trasnfer all css to a stylesheet classes
+
 
 
 counter = 0 ;
@@ -45,31 +47,31 @@ $(window).on("scroll" ,()=>{
         console.log("first triggered");
         $(".li-1 > div").css({
             "visibility":"visible",
-            "transition" : "0.8s"
+            "transition" : "opacity 2s ease-in"
         }) ;
+        $(".li-1 > div").fadeIn() ;
     }else if( scrollPos > 1250 &&  scrollPos < 1450) {
 
         console.log("second triggered");
         $(".li-2 > div").css({
             "visibility": "visible",
-            "transition": "0.8s"
+            "transition" : "opacity 2s ease-in"
         });
 
-    }else if( scrollPos > 1450 &&  scrollPos < 2000){
+    }else if( scrollPos > 1500 &&  scrollPos < 2000){
 
             console.log("second triggered");
             $(".li-3 > div").css({
                 "visibility":"visible",
-                "transition" : "0.2s"
+                "transition" : "opacity 2s ease-in"
             }) ;
-
 
 
 
     }else if(scrollPos < 500){
         $(".line > div").css({
             "visibility":"hidden",
-            "transition" : "0.2s"
+            "transition" : "opacity 2s ease-out"
         }) ;
         counter = 0 ;
 }
